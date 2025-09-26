063e0e0cecb005cdd78a3ea32bfca76639744043
bxxpe <ianphillips599@gmail.com>
2025-09-26 00:50:49 -0500

    Create Intro.swift

---
+++ Intro.swift
@@ -0,0 +1,16 @@
+#! /usr/bin/env swift
+
+import UIKit
+
+var greeting = "Hello, playground"
+greeting = "I can write Swift!"
+print(greeting)
+
+let myInt:Int = 5
+print(myInt)
+
+var myDouble:Double = 0.5
+print(myDouble)
+
+var myBool:Bool = true
+print(myBool)
