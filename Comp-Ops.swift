d79c43cdf720808ec497b52b4fda9c77cbc98e59
bxxpe <ianphillips599@gmail.com>
2025-09-26 02:10:18 -0500

    Create Comp-Ops.swift

---
+++ Comp-Ops.swift
@@ -0,0 +1,20 @@
+//
+// Using comparison Operators
+// Operators    Description
+// ==           Equal to
+// !=           Not Equal to
+// >            Greater than
+// <            Less than
+// >=           Greater than or Equal to
+// <=           Less than or Equal to
+//
+
+// Comparison Operators Example
+
+
+print(1 == 1)  // Equal to, true because 1 is equal to 1
+print(2 != 1)  // Not Equal to, true because 2 is not equal to 1
+print(2 > 1)   // Greater than, true because 2 is greater than 1
+print(1 < 2)   // Less than, true because 1 is less than 2
+print(1 >= 1)  // Greater than or Equal to, true because 1 is equal to 1
+print(2 <= 1)  // Less or Equal to, false because 2 is not less than or equal to 1
