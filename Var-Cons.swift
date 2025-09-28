fbf0679ecbe80846c61c0de7a984b5afd31a8bbc
bxxpe <ianphillips599@gmail.com>
2025-09-28 03:21:58 -0500

    Create Var-Cons.swift

---
+++ Var-Cons.swift
@@ -0,0 +1,26 @@
+/// Imagine you’re creating a photo sharing app and you want to keep track of the following metrics for each post:
+/// - Number of likes: The total number of likes the photo has received.
+/// - Number of comments: The total number of comments left by other users on the photo.
+/// - Year created: The year the post was created.
+/// - Month created: The month the post was created, represented as a number between 1 and 12.
+/// - Day created: The day of the month the post was created.
+
+var likes = 0          // changes over time
+var comments = 0       // changes over time
+let year = 2025        // fixed at creation
+let month = 9          // fixed at creation
+let day = 28           // fixed at creation
+
+
+/// Imagine a fitness tracking app needs to keep track of personal information in order to display the right information to the user:
+/// - Name: The user's first name.
+/// - Age: The user's age.
+/// - Number of steps taken today: The total number of steps the user inputs for the current day.
+/// - Goal number of steps: The user's daily step goal.
+/// - Average heart rate: The user's daily average heart rate.
+
+let name = "Ian"         // usually doesn’t change
+var age = 25              // changes yearly
+var stepsToday = 8500     // changes daily
+var goalSteps = 10000     // user can change goal
+var averageHeartRate = 72 // changes daily
