5e64345c2bd0b2eacd283788ad0075e194e3e9b2
bxxpe <ianphillips599@gmail.com>
2025-11-13 09:44:21 -0600

    Add while loop example in while.swift
    Add a while loop example in Swift with comments.

---
+++ while.swift
@@ -0,0 +1,13 @@
+//while loop
+
+//while condition == true {
+// code
+//}
+
+// condition is checked, and if true, the set of statements in the curly braces are executed
+
+var y = 100
+while y < 50 {
+y += 5
+print("y is \(y)")
+}
